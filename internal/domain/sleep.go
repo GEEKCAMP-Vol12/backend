a6914fcff19f74b5399e8a6f99a41765154fd85a
taku10101 <taku1011ht@gmail.com>
2024-08-10 04:37:34 +0900

    add domain

--- internal/domain/sleep.go
+++ internal/domain/sleep.go
@@ -1,6 +1,15 @@
 package domain
 
 
+type Sleep struct {
+    ID        string `json:"id"`
+    Score     int `json:"score"`
+    UserID    string `json:"user_id"`
+    CreatedAt string `json:"created_at"`
+    UpdatedAt string `json:"updated_at"`
+    
+}
+
 
 type CreateSleepRequest struct {
     Score     int `json:"score"`
--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,6 +1,14 @@
 package domain
 
 
+type User struct {
+    ID        string `json:"id"`
+    Name      string `json:"name"`
+    Icon      string `json:"icon"`
+    Age       int    `json:"age"`
+    CreatedAt string `json:"created_at"`
+    UpdatedAt string `json:"updated_at"`
+}
 type CreateUserRequest struct {
     ID        string       `json:"id"`
     Name      string    `json:"name"`
